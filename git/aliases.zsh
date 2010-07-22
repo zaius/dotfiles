0a19d16c4b42cbff3a7c77f3f81e2e0d19d02db2
David Kelso <david@kelso.id.au>
2010-07-21 18:53:59 -0700

    git aliases courtesy of holman

---
+++ git/aliases.zsh
@@ -0,0 +1,6 @@
+# Thanks to @holman
+alias gs='git status'
+alias gd='git diff'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gp='git push origin HEAD'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
