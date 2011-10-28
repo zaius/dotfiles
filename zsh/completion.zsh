ad26cdf93f4c2f3ff2cd24d57c13714e9a8ea95d
David Kelso <david@kelso.id.au>
2011-10-27 20:08:35 -0700

    Complete host names from ssh configs.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -11,3 +11,20 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+
+# Use ssh files for host completion. Otherwise zsh tries to use /etc/hosts.
+# Code from here:
+# http://serverfault.com/questions/170346/how-to-edit-command-completion-for-ssh-on-zsh
+h=()
+if [[ -r ~/.ssh/config ]]; then
+  h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
+fi
+if [[ -r ~/.ssh/known_hosts ]]; then
+  h=($h ${${${(f)"$(cat ~/.ssh/known_hosts{,2} || true)"}%%\ *}%%,*}) 2>/dev/null
+fi
+if [[ $#h -gt 0 ]]; then
+  zstyle ':completion:*:ssh:*' hosts $h
+  zstyle ':completion:*:slogin:*' hosts $h
+fi
+
