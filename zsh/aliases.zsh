d11a7c2293d7f1e13e1ed8a6858af960379f092d
David Kelso <david@kelso.id.au>
2012-02-21 14:12:50 -0800

    Fix path and less.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias socks="ssh -D localhost:8000 kelso.id.au"
 alias vi="vim"
 
+# http://superuser.com/questions/106637/less-command-clearing-screen-upon-exit-how-to-switch-it-off
+alias less="less -X -R --quit-if-one-screen"
+
