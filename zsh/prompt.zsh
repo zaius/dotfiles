3fffb5a3c3a65981a35502985e66484f89673bad
David Kelso <david@kelso.id.au>
2012-02-21 18:23:03 -0800

    Move some functionality from screen to tmux.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,57 +1,17 @@
-# Thanks to phil for all the title setting stuff
-# http://aperiodic.net/phil/prompt/
-
-preexec () {
-  if [[ "$TERM" == "screen" ]]; then
-    local CMD=${1[(wr)^(*=*|sudo|-*)]}
-    echo -n "\ek$CMD\e\\"
-  fi
-}
-
+autoload colors && colors
 
 setprompt () {
   setopt prompt_subst
 
-  # See if we can use colors.
-  autoload colors zsh/terminfo
-  if [[ "$terminfo[colors]" -ge 8 ]]; then
-    colors
-  fi
+  # Nesting vars between %{ and %} tells zsh to ignore them when determining
+  # prompt length. Would be nice to redefine color arrays with these specified.
 
-  for color in RED GREEN YELLOW BLUE MAGENTA CYAN WHITE; do
-    eval PR_$color='%{$terminfo[bold]$fg[${(L)color}]%}'
-    eval PR_LIGHT_$color='%{$fg[${(L)color}]%}'
-    (( count = $count + 1 ))
-  done
-  PR_NO_COLOUR="%{$terminfo[sgr0]%}"
-
-
-  # Decide if we need to set titlebar text.
-  case $TERM in
-  xterm*)
-    PR_TITLEBAR=$'%{\e]0;%(!.-=*[ROOT]*=- | .)%n@%m:%~ \a%}'
-    ;;
-  screen)
-    PR_TITLEBAR=$'%{\e_screen \005 (\005t) | %(!.-=[ROOT]=- | .)%n@%m:%~ \e\\%}'
-    ;;
-  *)
-    PR_TITLEBAR=''
-    ;;
-  esac
-    
-    
-    # Decide whether to set a screen title
-  if [[ "$TERM" == "screen" ]]; then
-    PR_STITLE=$'%{\ekzsh\e\\%}'
-  else
-    PR_STITLE=''
-  fi
-    
-    
-  # Finally, the prompt.
-  PROMPT='$PR_SET_CHARSET$PR_STITLE${(e)PR_TITLEBAR}\
-%(?..$PR_LIGHT_RED%?$PR_BLUE:)\
-$PR_CYAN%n@%m$PR_NO_COLOR:$PR_LIGHT_RED%~$PR_NO_COLOUR%# '
+  PROMPT='%{$fg[red]%}%?:\
+%{$fg_bold[cyan]%}%n\
+%{$fg_bold[red]%}@\
+%{$fg_bold[cyan]%}%m:\
+%{$fg_bold[red]%}%~\
+%{$fg_no_bold[default]%}%# '
 }
 
 setprompt
