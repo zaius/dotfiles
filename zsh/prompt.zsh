82a9c6b2375e20e191b2622fedf8714603386c62
David Kelso <david@kelso.id.au>
2012-02-23 12:47:22 -0800

    Chroot name in prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,15 +1,34 @@
 autoload colors && colors
 
+
+# Maybe look at tr for stripping out leading spaces to fix the shitty formatting
+# http://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-bash-variable
+exit_code () {
+  if [[ $? != 0 ]]; then
+    echo "%{$fg[red]%}%?\
+%{$fg[default]%}:"
+  fi
+}
+
+chroot () {
+  if [[ $SCHROOT_CHROOT_NAME != "" ]]; then
+    echo "%{$fg[red]%} in \
+%{$fg[magenta]%}$SCHROOT_CHROOT_NAME"
+  fi
+}
+
 setprompt () {
   setopt prompt_subst
 
   # Nesting vars between %{ and %} tells zsh to ignore them when determining
   # prompt length. Would be nice to redefine color arrays with these specified.
 
-  PROMPT='%{$fg[red]%}%?:\
+  PROMPT='$(exit_code)\
 %{$fg_bold[cyan]%}%n\
 %{$fg_bold[red]%}@\
-%{$fg_bold[cyan]%}%m:\
+%{$fg_bold[cyan]%}%m\
+$(chroot)\
+:\
 %{$fg_bold[red]%}%~\
 %{$fg_no_bold[default]%}%# '
 }
