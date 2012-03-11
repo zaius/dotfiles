9ceefafc746f7d9bb514a83f8382c0313a0f544d
David Kelso <david@kelso.id.au>
2012-03-11 15:12:56 -0700

    Keep environment with sudo.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,5 @@ alias vi="vim"
 # http://superuser.com/questions/106637/less-command-clearing-screen-upon-exit-how-to-switch-it-off
 alias less="less -X -R --quit-if-one-screen"
 
+# Keep user environment when using sudo
+alias sudo="sudo -E"
