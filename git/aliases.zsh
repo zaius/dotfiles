c470d321af4aa8feeed3efb3d08f8a976afabc29
David Kelso <david@kelso.id.au>
2012-08-13 11:36:15 -0700

    Git stash save.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,4 +4,7 @@ alias gd='git diff'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gp='git push origin HEAD'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+# Get to the top of the repo
 alias gcd='cd `git rev-parse --show-cdup`'
+# Stash everything except the index in order to run tests
+alias gss='git stash save --include-untracked --keep-index'
