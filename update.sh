a97a231af10114f8a051423285c5a07b941a9d79
David Kelso <david@kelso.id.au>
2012-10-25 21:57:58 -0700

    No more command-t.

--- update.sh
+++ update.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Update all submodules and compile command-t
-
+# Update all submodules
 git submodule init
 git submodule update
 git submodule foreach git pull origin master
-
-cd vim/vim.symlink/bundle/command-t/ruby/command-t
-ruby extconf.rb
-make
-sudo make install
