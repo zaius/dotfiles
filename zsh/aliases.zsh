abf95fe8257166f2e814392f5d42414e28b7146d
David Kelso <david@kelso.id.au>
2012-11-06 22:19:32 -0800

    Alias update.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,8 @@ alias socks="ssh -D localhost:8000 kelso.id.au"
 alias vi="vim"
 
 # http://superuser.com/questions/106637/less-command-clearing-screen-upon-exit-how-to-switch-it-off
-alias less="less -X -R --quit-if-one-screen"
+# -i case insensitive search
+alias less="less -i -X -R --quit-if-one-screen"
 
 # Keep user environment when using sudo
 alias sudo="sudo -E"
@@ -11,3 +12,7 @@ alias sudo="sudo -E"
 # Escape sequence is: ESC k name ESC \
 # These are literal esc chars - ctrl-v ESC
 title () { echo -n "k$@\\" }
+
+# plutil args are impossible to remember
+# alias p2j="plutil -convert json -o - -- - | python -mjson.tool"
+alias p2j="plutil -convert json -r -o - -- -"
