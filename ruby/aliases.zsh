18c4759b685c115a0bb7331256db5d76c405657c
David Kelso <david@kelso.id.au>
2012-11-07 17:54:35 -0800

    Note to future david.

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,4 +1,8 @@
 gem() {
+  # Oh hi future david! You're reading this because you typed `which gem` but
+  # gem is actually a function. You can remove it with 'unset -f gem' but this
+  # is probably what you want to do: command which gem
+
   if [[ $@ == "uninstall all" ]]; then
     command gem list --no-version | xargs gem uninstall -ax
   else
