d2ee4af105f9c5049600a88276a0d8dc9f57c033
David Kelso <david@kelso.id.au>
2012-11-13 11:28:02 -0800

    Run rake with bundle exec if we're in a bundled project.

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -9,3 +9,25 @@ gem() {
     command gem "$@";
   fi;
 }
+
+# Run rake from the bundle if it exists.
+#
+# It would be nice to do this with all executables inside a bundle, but I can't
+# find a nice way to get bundler to show me all the executables.
+# This gives a list of all executables in a bundle, but then I would have to run
+# it before every command. Could keep a cache?
+# ruby -e "require 'bundler'; puts Bundler.load.specs.map(&:executables).flatten.inspect"
+#
+# More ways to handle specs:
+# http://stackoverflow.com/questions/9322078/programmatically-determine-gems-path-using-bundler/9322317#9322317
+#
+# Rails does weird shit to make this not nescessary for the rails executable
+# http://yehudakatz.com/2011/05/30/gem-versioning-and-bundler-doing-it-right/
+rake() {
+  if [[ -e "./Gemfile.lock" ]] && `grep -q rake Gemfile.lock`; then
+    echo "Running rake from bundle"
+    command bundle exec rake "$@"
+  else
+    command rake "$@"
+  fi
+}
