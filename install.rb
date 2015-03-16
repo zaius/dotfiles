2705be836bdb95ecab62e517a5bc8c1fea2cbbfb
David Kelso <david@kelso.id.au>
2015-03-16 12:55:04 -0700

    Use absolute paths in installer.

--- install.rb
+++ install.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/ruby
 
-# In case I forgot to run clone --recursive
-`git submodule update --init`
-
 require 'fileutils'
 
 ROOT = File.expand_path('../', __FILE__)
 HOME = ENV['HOME']
+
+# In case I forgot to run clone --recursive
+`git -C #{ROOT} submodule update --init`
 
 # TODO: pop this off argv
 force = false
 
 
 # Copy files that can't be symlinks (e.g. .forward)
-Dir['**/**.copy'].each do |file|
+Dir["#{ROOT}/**/**.copy"].each do |file|
   output_file = HOME + '/.' + File.basename(file, '.copy')
   FileUtils.rm_rf output_file if File.exists?(output_file) || File.symlink?(output_file)
-  FileUtils.cp ROOT + '/' + file, output_file
+  FileUtils.cp file, output_file
 end
 
 # Link files into home directory
-Dir['**/**.symlink'].each do |file|
+Dir["#{ROOT}/**/**.symlink"].each do |file|
   next if file.include? 'undo'
 
   output_file = HOME + '/.' + File.basename(file, '.symlink')
@@ -28,7 +28,7 @@ Dir['**/**.symlink'].each do |file|
   if !force && File.exists?(output_file) && !File.symlink?(output_file)
 
     print "#{output_file} will be overwritten. Delete it? "
-    if gets.chomp.upcase.starts_with? 'Y'
+    if gets.chomp.upcase.start_with? 'Y'
       # Have to use recursive as it could be a directory (eg. vim)
       FileUtils.rm_rf output_file
     else
@@ -39,7 +39,7 @@ Dir['**/**.symlink'].each do |file|
   File.delete output_file if File.symlink? output_file
 
   puts "Linking #{file} to #{output_file}"
-  File.symlink ROOT + '/' + file, output_file
+  File.symlink file, output_file
 end
 
 # TODO: overwriting .ssh sucks. Want to leave what's in there, and maybe even
