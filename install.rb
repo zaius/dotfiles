809c889a781cc2befaf7669552c2989dcab8c89a
David Kelso <david@kelso.id.au>
2016-04-25 21:18:15 -0700

    Always overwrite dotfiles.

--- install.rb
+++ install.rb
@@ -5,13 +5,6 @@ require 'fileutils'
 ROOT = File.expand_path('../', __FILE__)
 HOME = ENV['HOME']
 
-# In case I forgot to run clone --recursive
-`git -C #{ROOT} submodule update --init`
-
-# TODO: pop this off argv
-force = false
-
-
 # Copy files that can't be symlinks (e.g. .forward)
 Dir["#{ROOT}/**/**.copy"].each do |file|
   output_file = HOME + '/.' + File.basename(file, '.copy')
@@ -26,16 +19,8 @@ Dir["#{ROOT}/**/**.symlink"].each do |file|
 
   output_file = HOME + '/.' + File.basename(file, '.symlink')
 
-  if !force && File.exists?(output_file) && !File.symlink?(output_file)
-
-    print "#{output_file} will be overwritten. Delete it? "
-    if gets.chomp.upcase.start_with? 'Y'
-      puts "Overwriting #{output_file}"
-      # Have to use recursive as it could be a directory (eg. vim)
-      FileUtils.rm_rf output_file
-    else
-      puts "Ignoring #{output_file}"
-    end
+  if File.exists?(output_file) && !File.symlink?(output_file)
+    FileUtils.rm_rf output_file
   end
 
   File.delete output_file if File.symlink? output_file
