1b9a68b1cf8c37d5c54e93464782cdf846cbc621
David Kelso <david@kelso.id.au>
2016-05-09 10:27:00 -0700

    Earlier escape of color code length.

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -51,16 +51,16 @@ man() {
 typeset -Ag FX FG BG
 
 FX=(
-  reset     "[00m"
-  bold      "[01m" no-bold      "[22m"
-  italic    "[03m" no-italic    "[23m"
-  underline "[04m" no-underline "[24m"
-  blink     "[05m" no-blink     "[25m"
-  reverse   "[07m" no-reverse   "[27m"
+  reset     "%{[0m%}"
+  bold      "%{[1m%}" no-bold      "%{[22m%}"
+  italic    "%{[3m%}" no-italic    "%{[23m%}"
+  underline "%{[4m%}" no-underline "%{[24m%}"
+  blink     "%{[5m%}" no-blink     "%{[25m%}"
+  reverse   "%{[7m%}" no-reverse   "%{[27m%}"
 )
 
 # Fill the color maps.
 for color in {000..256}; do
-  FG[$color]="[38;5;${color}m"
-  BG[$color]="[48;5;${color}m"
+  FG[$color]="%{[38;5;${color}m%}"
+  BG[$color]="%{[48;5;${color}m%}"
 done
