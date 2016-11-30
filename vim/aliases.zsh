ca087b4252a6d6c836f7f95e4f1f2b19de2c2c5d
David Kelso <david@kelso.id.au>
2016-11-29 23:28:21 -0800

    Switch default editor to vimr.

--- vim/aliases.zsh
+++ vim/aliases.zsh
@@ -1,9 +1,13 @@
 if [[ $platform == 'Darwin' ]]; then
-  alias vi='mvim'
-  alias vim='mvim'
+  alias vi='vimr'
+  alias vim='vimr'
+  # Stick with neovim until I can patch vimr to block
+  export EDITOR="nvim"
 elif which nvim > /dev/null; then
   alias vi='nvim'
   alias vim='nvim'
+  export EDITOR="nvim"
 elif which vim > /dev/null; then
   alias vi='vim'
+  export EDITOR="vim"
 fi
