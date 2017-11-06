3694bfb79ed4c5934168f0000a4d1d368b9d4613
David Kelso <david@kelso.id.au>
2017-11-06 15:36:23 -0800

    Git cached alias.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,6 +2,7 @@
 alias gs='git status -sb'
 alias wow='git status -sb'
 alias gd='git diff'
+alias gdc='git diff --cached'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gp='git push origin HEAD'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
