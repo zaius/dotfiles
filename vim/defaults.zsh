c6904ef1631c25dca368ddb1b611f594dc1a7b65
David Kelso <david@kel.so>
2020-07-26 10:52:19 -0700

    Smaller macvim tabs.

--- vim/defaults.zsh
+++ vim/defaults.zsh
@@ -4,4 +4,6 @@
 # http://code.google.com/p/macvim/wiki/UserDefaults
 if [[ $platform == 'Darwin' ]]; then
   defaults write org.vim.MacVim MMZoomBoth 1
+  # Allow more tabs
+  defaults write org.vim.MacVim MMTabMinWidth 50
 fi
